6b953142a3ff5f6f8aee75497fbbce45ffec9f71
Lilong Zhang <and2long@gmail.com>
2023-02-18 10:42:15 +0800

    fix httpRequestLogger error.

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,14 @@
+import bodyParser from "body-parser";
 import express, { Express, Request, Response, Router } from 'express';
 import { userRoute } from "./routes/user-route";
 import { httpRequestLogger } from './utils/loggers';
-import bodyParser from "body-parser";
 
 const app: Express = express();
-app.use(httpRequestLogger);
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
+app.use(httpRequestLogger);
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello, this is Express + TypeScript');
--- src/utils/loggers.ts
+++ src/utils/loggers.ts
@@ -1,12 +1,8 @@
-import { NextFunction } from "express";
+import { NextFunction, Request, Response } from 'express';
 import stringify from "fast-safe-stringify";
 import _ from "lodash";
 
 export const httpRequestLogger = (req: Request, res: Response, next: NextFunction): void => {
-  if (req.url.endsWith("/healthz")) {
-    next();
-    return;
-  }
   let msg = `-> Incoming Request ${req.method} ${req.url}`;
   if (!_.isEmpty(req.body)) {
     msg += `, Request Body: ${stringify(req.body)}`;
