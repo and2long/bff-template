b91f5538ea5ec8f8b44abbd0018c2f87fc3de2fb
Lilong Zhang <and2long@gmail.com>
2023-03-06 22:00:31 +0800

    add seeder file for doctor.

---
+++ src/seeders/20230306135408-doctor.js
@@ -0,0 +1,13 @@
+'use strict';
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('Doctors', [
+      { userId: '1b9d6bcd-bbfd-4b2d-9b5d-ab8dfbbd4bed', hospitalId: '1', departmentId: '1', levelId: '1' },
+    ], {});
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('Doctors', null, {});
+  }
+};
