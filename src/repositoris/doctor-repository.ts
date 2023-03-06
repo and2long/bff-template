0dff3d22abe1cae99825f320ea74a3fa110de7f8
Lilong Zhang <and2long@gmail.com>
2023-03-06 22:43:25 +0800

    Optimize doctor query condition.

--- src/repositoris/doctor-repository.ts
+++ src/repositoris/doctor-repository.ts
@@ -1,9 +1,12 @@
+import Department from "../entities/department";
 import Doctor from "../entities/doctor";
+import DoctorLevel from "../entities/doctor-level";
+import Hospital from "../entities/hospital";
 import { DoctorInterface } from "../interfaces/user";
 
 class DoctorRepository {
   public async findAll(): Promise<DoctorInterface[]> {
-    return Doctor.findAll();
+    return Doctor.findAll({ include: [Hospital, Department, DoctorLevel] });
   }
 }
 
