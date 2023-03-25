5859401b11c76c89d11e285afa673b5384944c77
Lilong Zhang <and2long@gmail.com>
2023-03-25 22:01:20 +0800

    Update keycloak config.

--- src/utils/keycloak-setup.ts
+++ src/utils/keycloak-setup.ts
@@ -4,19 +4,21 @@ import envConfig from "../config/env-config";
 
 export const KEYCLOAK_REALM = "qunai";
 export const KEYCLOAK_CLIENT_ID = "qunai-medical";
-export const KEYCLOAK_BASE_URL = "http://127.0.0.1:8080";
+export const KEYCLOAK_BASE_URL = "http://localhost:8080";
 
 const kcConfig = {
-  "confidential-port": 0,
-  "auth-server-url": KEYCLOAK_BASE_URL,
-  "resource": KEYCLOAK_CLIENT_ID,
-  "ssl-required": "external",
-  "bearer-only": true,
   "realm": KEYCLOAK_REALM,
+  "auth-server-url": KEYCLOAK_BASE_URL,
+  "ssl-required": "external",
+  "resource": KEYCLOAK_CLIENT_ID,
+  "verify-token-audience": false,
   "credentials": {
     "secret": envConfig.keycloakClientSecret
   },
-  "policy-enforcer": {}
+  "confidential-port": 0,
+  "policy-enforcer": {},
+  "bearer-only": true
 };
+
 export const memoryStore = new session.MemoryStore();
 export const keycloak = new Keycloak({ store: memoryStore }, kcConfig);
