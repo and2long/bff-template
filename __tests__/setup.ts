27f82661c934abbc108dcd6a6a9dff1a0d09525a
Lilong Zhang <and2long@gmail.com>
2023-04-24 21:53:50 +0800

    Fix unit test for appointment route protected by keycloak.

--- __tests__/setup.ts
+++ __tests__/setup.ts
@@ -1,4 +1,21 @@
 import { sequelize } from "../src/utils/db-setup";
+import { NextFunction, Request, Response } from "express";
+
+export const protectRouteSpy = jest.fn();
+
+jest.mock("keycloak-connect", () => {
+  return jest.fn().mockImplementation(() => {
+    return {
+      middleware: () => (req: Request, res: Response, next: NextFunction) => {
+        next();
+      },
+      protect: () => (req: Request, res: Response, next: NextFunction) => {
+        protectRouteSpy();
+        next();
+      }
+    };
+  });
+});
 
 beforeAll(async () => {
   await sequelize.authenticate();
